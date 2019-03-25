e2378adf0f6305a6a7d44f17a5743005d1322fa8
QuinicAcid <madica@yahoo.com>
2019-03-25 00:25:33 -0400

    First working Netbeans commit.

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php
-
-echo "Test: Start Page";
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        echo "Test: Start Page";
+        ?>
+    </body>
+</html>
