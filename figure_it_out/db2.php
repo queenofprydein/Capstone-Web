3761af744392bcf7628c3cc73d8fef9e95d6b230
QuinicAcid <madica@yahoo.com>
2019-04-13 11:24:59 -0400

    Set up page links.

---
+++ figure_it_out/db2.php
@@ -0,0 +1,14 @@
+<?php
+/* Connect to a MySQL database using driver invocation */
+//$dsn = 'sqlsrv:dbname=DB_A47087_smgroup;host=sql5008.site4now.net';
+$dsn = 'sqlsrv:Database=DB_A47087_smgroup;server=sql5008.site4now.net';
+$user = 'DB_A47087_smgroup_admin';
+$password = 'ftccgroup1';
+
+try {
+    $dbh = new PDO($dsn, $user, $password);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
+
+?>
