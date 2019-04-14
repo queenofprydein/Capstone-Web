191b4b5168456019fababf2c6c3377cdb70fe3c6
QuinicAcid <madica@yahoo.com>
2019-04-14 16:06:18 -0400

    Removed The Search for Spock.

--- db_connect.php
+++ db_connect.php
@@ -18,7 +18,7 @@ $dsn = 'sqlsrv:server='. $host .'; Database='. $dbname;
 // Create PDO instance
 $connection = new PDO($dsn, $user, $password);
 
-// Set default fetch action for $connection
+// Set default fetch action for $connection to object
 $connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 
 # PRDO QUERY
