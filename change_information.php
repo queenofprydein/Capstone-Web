1bd5beb446022dd7751130f3f777df9628931d86
QuinicAcid <madica@yahoo.com>
2019-04-25 19:36:23 -0400

    Validation and requirements.

--- change_information.php
+++ change_information.php
@@ -56,17 +56,17 @@ This program can be freely copied and/or distributed.
             echo "<h2>Change Whatever is Wrong.<br></h2>";
             echo 'Volunteer ID: ' . $result["Volunteer_ID"] . '<br>';
             echo '<label>Last_Name</label>';
-            echo 'Last Name: <input type="text" name="last_name" value="' . $result["Last_Name"] . '"><br>';
-            echo 'First Name:<input type="text" name="first_name" value=' . $result["First_Name"] . '><br>';
+            echo 'Last Name: <input type="text" name="last_name" required value="' . $result["Last_Name"] . '"><br>';
+            echo 'First Name:<input type="text" name="first_name" required value=' . $result["First_Name"] . '><br>';
             echo 'Middle Name:<input type="text" name="middle_name" value=' . $result["Middle_Name"] . '><br>';
             echo 'Phone:<input type="text" name="phone_number" value=' . $result["Phone"] . '><br>';
-            echo 'E-Mail:<input type="text" name="user_email" value=' . $result["Email"] . '><br>';
+            echo 'E-Mail:<input type="text" name="user_email" required value=' . $result["Email"] . '><br>';
             echo 'Preferred Method of contact:<input type="text" name="contact_method" value=' . $result["Preferred_Method_Of_Contact"] . '><br>';
-            echo 'Birth date:<input type="text" name="birth_date" value=' . $result["BirthDate"] . '><br>';
-            echo 'Gender:<input type="text" name="gender" value=' . $result["Gender"] . '><br>';
+            echo 'Birth date:<input type="text" name="birth_date" required value=' . $result["BirthDate"] . '><br>';
+            echo 'Gender:<input type="text" name="gender" required value=' . $result["Gender"] . '><br>';
             echo 'Emergency contact phone number:<input type="text" name="contact_phone" value=' . $result["Emergency_Contact_Phone"] . '><br>';
             echo 'Emergency contact name:<input type="text" name="contact_name" value=' . $result["Emergency_Contact_Name"] . '><br>';
-            echo 'Community service (y/n):<input type="text" name="community_service" value=' . $result["Community_Service"] . '><br>';
+            echo 'Community service (y/n):<input type="text" name="community_service" required value=' . $result["Community_Service"] . '><br>';
             ?>
             <br>
 
